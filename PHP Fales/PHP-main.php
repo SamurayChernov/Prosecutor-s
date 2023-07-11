d430831abc101931f6ec5d9e359fcd86515d0018
SamurayChernov <119618126+SamurayChernov@users.noreply.github.com>
2023-07-11 21:42:03 +0300

    Add files via upload

---
+++ PHP Fales/PHP-main.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link rel="stylesheet" href="./CSS/style-main.css">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+<title>Разработка сайта</title>
